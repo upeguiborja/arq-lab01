b6b9bd2fd7c2515a9587616c615da726231fa0e5
Mateo Upegui Borja <mateo@0x4d.dev>
2026-09-18 03:05:20 +0000

    chore: add java idioms

--- src/main/java/com/udea/banco/banco2025/entity/Customer.java
+++ src/main/java/com/udea/banco/banco2025/entity/Customer.java
@@ -6,11 +6,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "customers")
 public class Customer {
-    // POJO
-
     @Getter
     @Setter
     @Id
@@ -49,4 +49,28 @@ public class Customer {
         this.balance = balance;
     }
 
+
+    // Idioms
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
